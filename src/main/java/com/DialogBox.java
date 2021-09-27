d98565d7e16d2efe5a3583a62435c5753f7d0464
Rrraaaeee <qingxuannn0602@gmail.com>
2021-09-27 11:53:55 +0800

    checkstyle cleanup

--- src/main/java/com/DialogBox.java
+++ src/main/java/com/DialogBox.java
@@ -1,10 +1,5 @@
 package com;
-import javafx.geometry.Pos;
-import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
@@ -19,6 +14,9 @@ public class DialogBox extends HBox {
     private Label text;
     private ImageView displayPicture;
 
+    /**
+     * Constructor to set dialogbox parameters
+     */
     public DialogBox(Label l, ImageView iv) {
         text = l;
         displayPicture = iv;
@@ -31,9 +29,9 @@ public class DialogBox extends HBox {
         this.getChildren().addAll(text, displayPicture);
     }
 
-     /**
-     * Flips the dialog box such that the ImageView is on the left and text on the right.
-     */
+    /**
+    * Flips the dialog box such that the ImageView is on the left and text on the right.
+    */
     private void flip() {
         this.setAlignment(Pos.TOP_LEFT);
         ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
@@ -44,7 +42,7 @@ public class DialogBox extends HBox {
     public static DialogBox getUserDialog(Label l, ImageView iv) {
         return new DialogBox(l, iv);
     }
-    
+
     public static DialogBox getDukeDialog(Label l, ImageView iv) {
         var db = new DialogBox(l, iv);
         db.flip();
--- src/main/java/com/Duke.java
+++ src/main/java/com/Duke.java
@@ -14,22 +14,17 @@ import com.ui.Ui;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.text.Font;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
-import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.layout.Region;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-
-
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
 
 
 public class Duke extends Application {
@@ -38,6 +33,16 @@ public class Duke extends Application {
     private CommandHandler commandHandler;
     private CommandParser commandParser;
     private Storage storage;
+
+    // javafx
+    private Button sendButton;
+    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Scene scene;
+    private ScrollPane scrollPane;
+    private TextField userInput;
+    private VBox dialogContainer;
+
 
     /**
      * Default constructor needed to instantiate javafx
@@ -111,23 +116,15 @@ public class Duke extends Application {
         new Duke(storagePath).run();
     }
 
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-    private Image user = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image duke = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
-
 
 
 
     // https://se-education.org/guides/tutorials/javaFx.html
     @Override
     public void start(Stage stage) {
-         //Step 1. Setting up required components
-         
-        //The container for the content of the chat to scroll.
+        // Step 1. Setting up required components
+
+        // The container for the content of the chat to scroll.
         scrollPane = new ScrollPane();
         dialogContainer = new VBox();
         scrollPane.setContent(dialogContainer);
@@ -137,12 +134,12 @@ public class Duke extends Application {
         sendButton = new Button("Send");
         sendButton.setFont(new Font("Arial", 11));
         userInput.setFont(new Font("Arial", 11));
-     
+
         AnchorPane mainLayout = new AnchorPane();
         mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
 
         scene = new Scene(mainLayout);
-     
+
         stage.setScene(scene);
         stage.show();
 
@@ -151,38 +148,37 @@ public class Duke extends Application {
         stage.setResizable(false);
         stage.setMinHeight(600.0);
         stage.setMinWidth(400.0);
-        
+
         mainLayout.setPrefSize(400.0, 600.0);
-        
+
         scrollPane.setPrefSize(385, 535);
         scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
         scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-        
+
         scrollPane.setVvalue(1.0);
         scrollPane.setFitToWidth(true);
-        
-        // You will need to import `javafx.scene.layout.Region` for this. 
+
+        // You will need to import `javafx.scene.layout.Region` for this.
         dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-        
+
         userInput.setPrefWidth(325.0);
-        
+
         sendButton.setPrefWidth(55.0);
-        
+
         AnchorPane.setTopAnchor(scrollPane, 1.0);
-        
+
         AnchorPane.setBottomAnchor(sendButton, 1.0);
         AnchorPane.setRightAnchor(sendButton, 1.0);
-        
+
         AnchorPane.setLeftAnchor(userInput , 1.0);
         AnchorPane.setBottomAnchor(userInput, 1.0);
 
-
-       //Step 3. Add functionality to handle user input.
+        // Step 3. Add functionality to handle user input.
         sendButton.setOnMouseClicked((event) -> {
             dialogContainer.getChildren().add(getDialogLabel(userInput.getText()));
             userInput.clear();
         });
-    
+
         userInput.setOnAction((event) -> {
             dialogContainer.getChildren().add(getDialogLabel(userInput.getText()));
             userInput.clear();
@@ -217,10 +213,9 @@ public class Duke extends Application {
         Label textToAdd = new Label(text);
         textToAdd.setFont(new Font("Arial", 11));
         textToAdd.setWrapText(true);
-    
         return textToAdd;
     }
-    
+
     /**
      * Iteration 2:
      * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
@@ -237,7 +232,7 @@ public class Duke extends Application {
         );
         userInput.clear();
     }
-    
+
     /**
      * You should have your own function to generate a response to user input.
      * Replace this stub with your completed method.
