a85e3dedda544a2d26d5862d56dd5022bcfdf44c
Rrraaaeee <qingxuannn0602@gmail.com>
2021-09-30 16:45:24 +0800

    bug fix

--- src/main/java/com/command/MarkCommand.java
+++ src/main/java/com/command/MarkCommand.java
@@ -15,11 +15,15 @@ public class MarkCommand extends Command {
      */
     public void execute(TaskManager taskManager, Storage storage, Ui ui) {
         Integer taskId = Integer.parseInt(this.getTaskDescription().strip());
-        ui.showText("I have helped you marked your task: \n" + taskManager.markTaskDone(taskId));
-        ui.showSeperator();
-
+        Integer taskListSize = taskManager.getTaskListSize();
+        if (taskId > taskListSize || taskId < 1) {
+            ui.showText("The task ID you just keyed in is invalid");
+            return;
+        } else {
+            ui.showText("I have helped you marked your task: \n" + taskManager.markTaskDone(taskId));
+            ui.showSeperator();
+        }
     }
-
 }
 
 
